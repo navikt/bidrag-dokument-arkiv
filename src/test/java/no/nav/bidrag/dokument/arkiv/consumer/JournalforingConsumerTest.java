6ec151863516fe3b6117751af7d5743a969bb889
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-03-12 11:44:52 +0100

    remove error test with no functional meaning

--- src/test/java/no/nav/bidrag/dokument/arkiv/consumer/JournalforingConsumerTest.java
+++ src/test/java/no/nav/bidrag/dokument/arkiv/consumer/JournalforingConsumerTest.java
@@ -1,15 +1,9 @@
 package no.nav.bidrag.dokument.arkiv.consumer;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import no.nav.bidrag.dokument.arkiv.BidragDokumentArkivConfig;
 import no.nav.bidrag.dokument.arkiv.BidragDokumentArkivLocal;
 import no.nav.security.oidc.context.OIDCRequestContextHolder;
-import no.nav.security.oidc.context.OIDCValidationContext;
-import no.nav.security.oidc.context.TokenContext;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,23 +26,5 @@ class JournalforingConsumerTest {
   void skalFeileUtenBearerToken() {
     assertThatIllegalStateException().isThrownBy(() -> journalforingConsumer.hentJournalforing(1001))
         .withMessage("Kunne ikke videresende Bearer token");
-  }
-
-  @Test
-  @DisplayName("skal feile med ugyldig Bearer token")
-  void skalFeileMedUgyldigBearerToken() {
-    mockBearerToken();
-
-    Assertions.assertThatCode(() -> journalforingConsumer.hentJournalforing(1001))
-        .hasMessage("403 Forbidden");
-  }
-
-  private void mockBearerToken() {
-    OIDCValidationContext oidcValidationContextMock = mock(OIDCValidationContext.class);
-    TokenContext tokenContextMock = mock(TokenContext.class);
-
-    when(oidcRequestContextHolderMock.getOIDCValidationContext()).thenReturn(oidcValidationContextMock);
-    when(oidcValidationContextMock.getToken(BidragDokumentArkivConfig.ISSUER)).thenReturn(tokenContextMock);
-    when(tokenContextMock.getIdToken()).thenReturn("as safe as it is gonna get!!!");
   }
 }
