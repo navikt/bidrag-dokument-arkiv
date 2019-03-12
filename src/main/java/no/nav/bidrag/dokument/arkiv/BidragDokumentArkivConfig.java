6032077fc33367e24b4360c468e479c56c532edc
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-03-12 10:04:26 +0100

    CorrelationIdFilter

--- src/main/java/no/nav/bidrag/dokument/arkiv/BidragDokumentArkivConfig.java
+++ src/main/java/no/nav/bidrag/dokument/arkiv/BidragDokumentArkivConfig.java
@@ -1,5 +1,6 @@
 package no.nav.bidrag.dokument.arkiv;
 
+import no.nav.bidrag.commons.web.CorrelationIdFilter;
 import no.nav.bidrag.dokument.arkiv.consumer.JournalforingConsumer;
 import no.nav.security.oidc.context.OIDCRequestContextHolder;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,5 +21,10 @@ public class BidragDokumentArkivConfig {
   JournalforingConsumer journalforingConsumer(RestTemplate restTemplate, OIDCRequestContextHolder oidcRequestContextHolder) {
     restTemplate.setUriTemplateHandler(new RootUriTemplateHandler(baseUrl));
     return new JournalforingConsumer(oidcRequestContextHolder, restTemplate);
+  }
+
+  @Bean
+  public CorrelationIdFilter correlationIdFilter() {
+    return new CorrelationIdFilter();
   }
 }
