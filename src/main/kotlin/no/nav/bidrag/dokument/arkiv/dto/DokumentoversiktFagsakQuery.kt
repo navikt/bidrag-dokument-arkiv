11b807744da07d08e7359fb58360b0bdc5525637
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-04-10 15:07:22 +0200

    må ha verdi for paginering

--- src/main/kotlin/no/nav/bidrag/dokument/arkiv/dto/DokumentoversiktFagsakQuery.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/arkiv/dto/DokumentoversiktFagsakQuery.kt
@@ -3,7 +3,7 @@ package no.nav.bidrag.dokument.arkiv.dto
 data class DokumentoversiktFagsakQuery(val saksnummer: String) : GraphQuery() {
     private val query = """
 
-      dokumentoversiktFagsak(fagsak: {fagsakId: "$saksnummer", fagsaksystem: "BI01"}) {
+      dokumentoversiktFagsak(fagsak: {fagsakId: "$saksnummer", fagsaksystem: "BI01", foerste: 50}) {
         journalposter {
           journalpostId
           avsenderMottaker {
