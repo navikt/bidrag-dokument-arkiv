d7c569ffc2ce59f90ca91a646879e42f39af6cde
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-05-12 08:50:38 +0200

    srvvdarkiv -> srvbdarkiv, and grammar

--- init-scripts/init_srvbdarkiv.sh
+++ init-scripts/init_srvbdarkiv.sh
@@ -4,10 +4,10 @@ export SRV_BD_ARKIV_AUTH="No authentication available"
 
 if test -f /var/run/secrets/dev/srvbdarkiv/password; then
     SRV_BD_ARKIV_AUTH=$(cat /var/run/secrets/dev/srvbdarkiv/password)
-    echo Exporting authentication for srvbdarkiv for dev cluster
+    echo Exporting srvbdarkiv authentication for dev cluster
 else
   if test -f /var/run/secrets/prod/srvbdarkiv/password; then
     SRV_BD_ARKIV_AUTH=$(cat /var/run/secrets/prod/srvbdarkiv/password)
-    echo Exporting authentication for srvbdarkiv for prod cluster
+    echo Exporting srvbdarkiv authentication for prod cluster
   fi
 fi
