f760e20ae902cf02bbb75f702e83d378091126f0
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-05-15 12:29:48 +0200

    apply saf-query with no linefeeds
    - fjeret død kode (secret.properties)

--- src/main/kotlin/no/nav/bidrag/dokument/arkiv/dto/GraphQuery.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/arkiv/dto/GraphQuery.kt
@@ -8,6 +8,6 @@ abstract class GraphQuery {
             {
               "query":"${getQuery()}"
             }
-        """.trimIndent()
+        """.replace("\n", "").replace(" ", "").trim()
     }
 }
