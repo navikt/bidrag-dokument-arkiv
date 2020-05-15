1e60e6d1724c4466b108d7b5d70c84a7560e4338
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-05-15 08:35:53 +0200

    path to password file

--- init-scripts/init_srvbdarkiv.sh
+++ init-scripts/init_srvbdarkiv.sh
@@ -2,7 +2,7 @@
 
 export SRV_BD_ARKIV_AUTH="No authentication available"
 
-PATH_PASSWORD=/var/run/secrets/nais.io/srvbdarkiv
+PATH_PASSWORD=/var/run/secrets/nais.io/srvbdarkiv/password
 
 if test -f "$PATH_PASSWORD"; then
   SRV_BD_ARKIV_AUTH=$(cat "$PATH_PASSWORD")
