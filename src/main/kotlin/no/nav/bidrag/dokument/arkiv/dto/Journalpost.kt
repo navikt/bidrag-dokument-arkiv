68f209a90b1836856dd3643ed10767ee17e7ac46
Ugur Cenar <cenar93@gmail.com>
2022-02-09 14:19:14 +0100

    Ekspedert registrer retur

--- src/main/kotlin/no/nav/bidrag/dokument/arkiv/dto/Journalpost.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/arkiv/dto/Journalpost.kt
@@ -181,7 +181,7 @@ data class Journalpost(
 
     fun tilAvvik(): List<AvvikType> {
         val avvikTypeList = mutableListOf<AvvikType>()
-        if (isUtgaaendeDokument() && isStatusFerdigsstilt()) avvikTypeList.add(AvvikType.REGISTRER_RETUR)
+        if (isUtgaaendeDokument() && (isStatusFerdigsstilt() || isStatusEkspedert())) avvikTypeList.add(AvvikType.REGISTRER_RETUR)
         if (isStatusMottatt() && isInngaaendeDokument()) avvikTypeList.add(AvvikType.OVERFOR_TIL_ANNEN_ENHET)
         if (isStatusMottatt()) avvikTypeList.add(AvvikType.TREKK_JOURNALPOST)
         if (!isStatusMottatt() && hasSak() && !isStatusFeilregistrert()) avvikTypeList.add(AvvikType.FEILFORE_SAK)
