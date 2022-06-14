ae1a4ce3f6476327a26d03a6751603342d311606
Ugur Cenar <cenar93@gmail.com>
2022-06-14 13:46:47 +0200

    Oppgave retur oppgave opprettet hendelse fix

--- src/main/java/no/nav/bidrag/dokument/arkiv/kafka/HendelseListener.java
+++ src/main/java/no/nav/bidrag/dokument/arkiv/kafka/HendelseListener.java
@@ -36,9 +36,9 @@ public class HendelseListener {
   public void lesOppgaveOpprettetHendelse(ConsumerRecord<String, String> consumerRecord) {
     OppgaveHendelse oppgaveOpprettetHendelse = jsonMapperService.mapOppgaveHendelse(consumerRecord.value());
 
-    if (oppgaveOpprettetHendelse.erTemaBIDEllerFAR() && oppgaveOpprettetHendelse.erReturOppgave() && oppgaveOpprettetHendelse.erStatusOpprettet()) {
-        LOGGER.info("Mottatt retur oppgave opprettet hendelse med journalpostId {}, oppgaveId {}, tema {}, opprettetAv {}, og status {}",
-          oppgaveOpprettetHendelse.getJournalpostId(), oppgaveOpprettetHendelse.getId(), oppgaveOpprettetHendelse.getTema(), oppgaveOpprettetHendelse.getOpprettetAv(), oppgaveOpprettetHendelse.getStatus());
+    if (oppgaveOpprettetHendelse.erTemaBIDEllerFAR() && oppgaveOpprettetHendelse.erReturOppgave()) {
+        LOGGER.info("Mottatt retur oppgave opprettet hendelse med journalpostId {}, oppgaveId {}, tema {}, opprettetAv {}, saksnummer {}. statuskategori {} og status {}",
+          oppgaveOpprettetHendelse.getJournalpostId(), oppgaveOpprettetHendelse.getId(), oppgaveOpprettetHendelse.getTema(), oppgaveOpprettetHendelse.getOpprettetAv(), oppgaveOpprettetHendelse.getSaksreferanse(), oppgaveOpprettetHendelse.getStatuskategori(), oppgaveOpprettetHendelse.getStatus());
       behandleOppgaveHendelseService.behandleReturOppgaveOpprettetHendelse(oppgaveOpprettetHendelse);
     } else if (oppgaveOpprettetHendelse.erTemaBIDEllerFAR() && oppgaveOpprettetHendelse.erJournalforingOppgave()){
       LOGGER.info("Mottatt journalforing oppgave opprettet hendelse med journalpostId {}, oppgaveId {}, tema {}, opprettetAv {}, tildeltEnhetsnr {}, og status {}",
