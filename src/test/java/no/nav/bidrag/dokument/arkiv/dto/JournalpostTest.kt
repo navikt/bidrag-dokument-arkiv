a1c035bc295688f704ba9edb309c934641ab21ab
Ugur Alpay Cenar <cenar93@gmail.com>
2022-08-12 10:52:03 +0200

    Support for non-bidrag journalpost (#324)

--- src/test/java/no/nav/bidrag/dokument/arkiv/dto/JournalpostTest.kt
+++ src/test/java/no/nav/bidrag/dokument/arkiv/dto/JournalpostTest.kt
@@ -166,6 +166,7 @@ internal class JournalpostTest {
     @DisplayName("skal hente avvik hvis Journalpost med status mottatt og inngående")
     fun skalHenteAvvikForMottattOgInngaaende() {
         val journalpost = Journalpost()
+        journalpost.tema = "BID"
         journalpost.journalstatus = JournalStatus.MOTTATT
         journalpost.journalposttype = JournalpostType.I
         val avvikListe = journalpost.tilAvvik()
@@ -182,6 +183,7 @@ internal class JournalpostTest {
         journalpost.journalstatus = JournalStatus.FERDIGSTILT
         journalpost.journalposttype = JournalpostType.U
         journalpost.sak = Sak("")
+        journalpost.tema = "BID"
         val avvikListe = journalpost.tilAvvik()
         assertThat(avvikListe).hasSize(2)
         assertThat(avvikListe).contains(AvvikType.MANGLER_ADRESSE)
@@ -192,6 +194,7 @@ internal class JournalpostTest {
     @DisplayName("skal hente avvik hvis Journalpost er status er EKSPEDERT og utgående")
     fun skalHenteAvvikForEKSPEDERT_Utgaaende() {
         val journalpost = Journalpost()
+        journalpost.tema = "BID"
         journalpost.journalstatus = JournalStatus.EKSPEDERT
         journalpost.journalposttype = JournalpostType.U
         journalpost.antallRetur = 1
@@ -209,6 +212,7 @@ internal class JournalpostTest {
         journalpost.journalstatus = JournalStatus.JOURNALFOERT
         journalpost.journalposttype = JournalpostType.I
         journalpost.sak = Sak("")
+        journalpost.tema = "BID"
         val avvikListe = journalpost.tilAvvik()
         assertThat(avvikListe).hasSize(3)
         assertThat(avvikListe).contains(AvvikType.FEILFORE_SAK)
