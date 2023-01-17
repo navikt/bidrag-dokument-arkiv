990cecad2acc58876abb6366b9d0e6c4d6d49f93
Ugur Cenar <cenar93@gmail.com>
2023-01-17 07:35:29 +0100

    Hent dokument metadata refactor

--- src/main/java/no/nav/bidrag/dokument/arkiv/service/DokumentService.kt
+++ src/main/java/no/nav/bidrag/dokument/arkiv/service/DokumentService.kt
@@ -2,6 +2,7 @@ package no.nav.bidrag.dokument.arkiv.service
 
 import no.nav.bidrag.dokument.arkiv.consumer.SafConsumer
 import no.nav.bidrag.dokument.arkiv.dto.JournalStatus
+import no.nav.bidrag.dokument.arkiv.dto.TilknyttetJournalpost
 import no.nav.bidrag.dokument.arkiv.model.Discriminator
 import no.nav.bidrag.dokument.arkiv.model.ResourceByDiscriminator
 import no.nav.bidrag.dokument.dto.DokumentArkivSystemDto
@@ -30,35 +31,26 @@ class DokumentService(
         return safConsumer.hentDokument(journalpostId, java.lang.Long.valueOf(dokumentReferanse))
     }
 
+    fun tilÅpneDokumentMetadata(journalStatus: JournalStatus?, dokumentReferanse: String?, journalpostId: Long?) = ÅpneDokumentMetadata(
+        arkivsystem = DokumentArkivSystemDto.JOARK,
+        dokumentreferanse = dokumentReferanse,
+        journalpostId = "JOARK-$journalpostId",
+        format = DokumentFormatDto.PDF,
+        status = when(journalStatus){
+            JournalStatus.RESERVERT, JournalStatus.UNDER_ARBEID -> DokumentStatusDto.UNDER_REDIGERING
+            JournalStatus.AVBRUTT, JournalStatus.FEILREGISTRERT, JournalStatus.UTGAAR -> DokumentStatusDto.AVBRUTT
+            else -> DokumentStatusDto.FERDIGSTILT
+        }
+    )
     fun hentDokumentMetadata(journalpostId: Long? = null, dokumentReferanse: String?): List<ÅpneDokumentMetadata> {
         if (journalpostId == null && dokumentReferanse != null){
             return listOf(journalpostService.finnTilknyttedeJournalposter(dokumentReferanse)
-                .map { ÅpneDokumentMetadata(
-                    arkivsystem = DokumentArkivSystemDto.JOARK,
-                    dokumentreferanse = dokumentReferanse,
-                    journalpostId = "JOARK-${it.journalpostId}",
-                    format = DokumentFormatDto.PDF,
-                    status = when(it.journalstatus){
-                        JournalStatus.RESERVERT, JournalStatus.UNDER_ARBEID -> DokumentStatusDto.UNDER_REDIGERING
-                        JournalStatus.AVBRUTT, JournalStatus.FEILREGISTRERT, JournalStatus.UTGAAR -> DokumentStatusDto.AVBRUTT
-                        else -> DokumentStatusDto.FERDIGSTILT
-                    }
-                ) }.first())
+                .map { tilÅpneDokumentMetadata(it.journalstatus, dokumentReferanse, it.journalpostId) }.first())
         }
 
         val journalpost = journalpostService.hentJournalpost(journalpostId).orElse(null)
         return journalpost?.dokumenter?.map {
-            ÅpneDokumentMetadata(
-                arkivsystem = DokumentArkivSystemDto.JOARK,
-                dokumentreferanse = it.dokumentInfoId,
-                journalpostId = "JOARK-$journalpostId",
-                format = DokumentFormatDto.PDF,
-                status = when(journalpost.journalstatus){
-                    JournalStatus.RESERVERT, JournalStatus.UNDER_ARBEID -> DokumentStatusDto.UNDER_REDIGERING
-                    JournalStatus.AVBRUTT, JournalStatus.FEILREGISTRERT, JournalStatus.UTGAAR -> DokumentStatusDto.AVBRUTT
-                    else -> DokumentStatusDto.FERDIGSTILT
-                }
-            )
+            tilÅpneDokumentMetadata(journalpost.journalstatus, it.dokumentInfoId, journalpostId)
         }?.filter { dokumentReferanse.isNullOrEmpty() || it.dokumentreferanse == dokumentReferanse } ?: emptyList()
     }
 
