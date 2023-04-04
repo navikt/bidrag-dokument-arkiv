0be8e601abf3366a0e3a51d711e81848abcabedf
Bent J. Lorentzen <bent.lorentzen@nav.no>
2023-04-04 16:06:50 +0200

    Byttet personRequest med personIdent

--- src/main/java/no/nav/bidrag/dokument/arkiv/consumer/PersonConsumer.java
+++ src/main/java/no/nav/bidrag/dokument/arkiv/consumer/PersonConsumer.java
@@ -6,8 +6,8 @@ import static no.nav.bidrag.dokument.arkiv.CacheConfig.PERSON_CACHE;
 
 import java.util.Optional;
 import no.nav.bidrag.commons.web.HttpHeaderRestTemplate;
-import no.nav.bidrag.dokument.arkiv.dto.PersonRequest;
 import no.nav.bidrag.dokument.arkiv.model.PersonException;
+import no.nav.bidrag.domain.ident.PersonIdent;
 import no.nav.bidrag.transport.person.PersonAdresseDto;
 import no.nav.bidrag.transport.person.PersonDto;
 import org.slf4j.Logger;
@@ -34,8 +34,8 @@ public class PersonConsumer {
   @Retryable(value = Exception.class, maxAttempts = 5, backoff = @Backoff(delay = 500, maxDelay = 3000, multiplier = 2.0))
   public Optional<PersonDto> hentPerson(String id){
     try {
-      var personResponse = restTemplate.exchange("/informasjon", HttpMethod.POST, new HttpEntity<>(new PersonRequest(id, id)), PersonDto.class);
-      if (HttpStatus.NO_CONTENT.equals(personResponse.getStatusCode())){
+      var personResponse = restTemplate.exchange("/informasjon", HttpMethod.POST, new HttpEntity<>(new PersonIdent(id)), PersonDto.class);
+      if (HttpStatus.NO_CONTENT == personResponse.getStatusCode()){
         return Optional.empty();
       }
 
@@ -49,7 +49,7 @@ public class PersonConsumer {
 
   @Cacheable(value = PERSON_ADRESSE_CACHE, unless = "#result == null")
   public PersonAdresseDto hentAdresse(String id){
-    return restTemplate.exchange("/adresse/post", HttpMethod.POST, new HttpEntity<>(new PersonRequest(id, id)), PersonAdresseDto.class).getBody();
+    return restTemplate.exchange("/adresse/post", HttpMethod.POST, new HttpEntity<>(new PersonIdent(id)), PersonAdresseDto.class).getBody();
   }
 
   public void leggTilInterceptor(ClientHttpRequestInterceptor requestInterceptor) {
