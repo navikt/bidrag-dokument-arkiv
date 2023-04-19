c0bd94ec4a03f28aa4d9571259b3eaa790a2994d
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2023-04-19 08:51:21 +0200

    Byttet lokale transportobjekter for person med de fra bidrag-transport (#374)

--- src/main/java/no/nav/bidrag/dokument/arkiv/consumer/PersonConsumer.java
+++ src/main/java/no/nav/bidrag/dokument/arkiv/consumer/PersonConsumer.java
@@ -6,10 +6,11 @@ import static no.nav.bidrag.dokument.arkiv.CacheConfig.PERSON_CACHE;
 
 import java.util.Optional;
 import no.nav.bidrag.commons.web.HttpHeaderRestTemplate;
-import no.nav.bidrag.dokument.arkiv.dto.PersonRequest;
 import no.nav.bidrag.dokument.arkiv.model.PersonException;
+import no.nav.bidrag.domain.ident.PersonIdent;
 import no.nav.bidrag.transport.person.PersonAdresseDto;
 import no.nav.bidrag.transport.person.PersonDto;
+import no.nav.bidrag.transport.person.PersonRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
@@ -23,6 +24,7 @@ import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 public class PersonConsumer {
+
   private final RestTemplate restTemplate;
   private static final Logger LOGGER = LoggerFactory.getLogger(PersonConsumer.class);
 
@@ -32,15 +34,18 @@ public class PersonConsumer {
 
   @Cacheable(value = PERSON_CACHE, unless = "#result==null")
   @Retryable(value = Exception.class, maxAttempts = 5, backoff = @Backoff(delay = 500, maxDelay = 3000, multiplier = 2.0))
-  public Optional<PersonDto> hentPerson(String id){
+  public Optional<PersonDto> hentPerson(String id) {
     try {
-      var personResponse = restTemplate.exchange("/informasjon", HttpMethod.POST, new HttpEntity<>(new PersonRequest(id, id)), PersonDto.class);
-      if (HttpStatus.NO_CONTENT.equals(personResponse.getStatusCode())){
+      var personResponse = restTemplate.exchange("/informasjon",
+          HttpMethod.POST,
+          new HttpEntity<>(new PersonRequest(new PersonIdent(id))),
+          PersonDto.class);
+      if (HttpStatus.NO_CONTENT == personResponse.getStatusCode()) {
         return Optional.empty();
       }
 
       return Optional.ofNullable(personResponse.getBody());
-    } catch (HttpStatusCodeException e){
+    } catch (HttpStatusCodeException e) {
       LOGGER.warn("Det skjedde en feil ved henting av person", e);
       SECURE_LOGGER.warn("Det skjedde en feil ved henting av person {}", id, e);
       throw new PersonException("Det skjedde en feil ved henting av person");
@@ -48,8 +53,8 @@ public class PersonConsumer {
   }
 
   @Cacheable(value = PERSON_ADRESSE_CACHE, unless = "#result == null")
-  public PersonAdresseDto hentAdresse(String id){
-    return restTemplate.exchange("/adresse/post", HttpMethod.POST, new HttpEntity<>(new PersonRequest(id, id)), PersonAdresseDto.class).getBody();
+  public PersonAdresseDto hentAdresse(String id) {
+    return restTemplate.exchange("/adresse/post", HttpMethod.POST, new HttpEntity<>(new PersonIdent(id)), PersonAdresseDto.class).getBody();
   }
 
   public void leggTilInterceptor(ClientHttpRequestInterceptor requestInterceptor) {
