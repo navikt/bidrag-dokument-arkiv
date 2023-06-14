e5d42e96e76ba3bf67b8c5947db06b1a795e4e26
Ugur Cenar <cenar93@gmail.com>
2023-06-14 07:34:01 +0200

    Enable cache bugfix

--- src/main/java/no/nav/bidrag/dokument/arkiv/CacheConfig.java
+++ src/main/java/no/nav/bidrag/dokument/arkiv/CacheConfig.java
@@ -1,6 +1,5 @@
 package no.nav.bidrag.dokument.arkiv;
 
-
 import com.github.benmanes.caffeine.cache.Caffeine;
 import java.util.concurrent.TimeUnit;
 import org.springframework.cache.CacheManager;
@@ -12,7 +11,7 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 @EnableCaching
-@Profile("live")
+@Profile("nais")
 public class CacheConfig {
 
   public static final String STS_SERVICE_USER_TOKEN_CACHE = "STS_SERVICE_USER_TOKEN_CACHE";
@@ -25,42 +24,21 @@ public class CacheConfig {
   @Bean
   public CacheManager cacheManager() {
     CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
-    caffeineCacheManager.registerCustomCache(STS_SERVICE_USER_TOKEN_CACHE,
-        Caffeine.newBuilder()
-            .expireAfterWrite(50, TimeUnit.MINUTES)
-            .recordStats()
-            .build()
-    );
-    caffeineCacheManager.registerCustomCache(PERSON_CACHE,
-        Caffeine.newBuilder()
-            .expireAfterWrite(24, TimeUnit.HOURS)
-            .recordStats()
-            .build()
-    );
-    caffeineCacheManager.registerCustomCache(PERSON_ADRESSE_CACHE,
-        Caffeine.newBuilder()
-            .expireAfterWrite(24, TimeUnit.HOURS)
-            .recordStats()
-            .build()
-    );
-    caffeineCacheManager.registerCustomCache(GEOGRAFISK_ENHET_CACHE,
-        Caffeine.newBuilder()
-            .expireAfterWrite(24, TimeUnit.HOURS)
-            .recordStats()
-            .build()
-    );
-    caffeineCacheManager.registerCustomCache(GEOGRAFISK_ENHET_WITH_TEMA_CACHE,
-        Caffeine.newBuilder()
-            .expireAfterWrite(24, TimeUnit.HOURS)
-            .recordStats()
-            .build()
-    );
-    caffeineCacheManager.registerCustomCache(SAKSBEHANDLERINFO_CACHE,
-        Caffeine.newBuilder()
-            .expireAfterWrite(24, TimeUnit.HOURS)
-            .recordStats()
-            .build()
-    );
+    caffeineCacheManager.registerCustomCache(
+        PERSON_CACHE,
+        Caffeine.newBuilder().expireAfterWrite(24, TimeUnit.HOURS).recordStats().build());
+    caffeineCacheManager.registerCustomCache(
+        PERSON_ADRESSE_CACHE,
+        Caffeine.newBuilder().expireAfterWrite(24, TimeUnit.HOURS).recordStats().build());
+    caffeineCacheManager.registerCustomCache(
+        GEOGRAFISK_ENHET_CACHE,
+        Caffeine.newBuilder().expireAfterWrite(24, TimeUnit.HOURS).recordStats().build());
+    caffeineCacheManager.registerCustomCache(
+        GEOGRAFISK_ENHET_WITH_TEMA_CACHE,
+        Caffeine.newBuilder().expireAfterWrite(24, TimeUnit.HOURS).recordStats().build());
+    caffeineCacheManager.registerCustomCache(
+        SAKSBEHANDLERINFO_CACHE,
+        Caffeine.newBuilder().expireAfterWrite(24, TimeUnit.HOURS).recordStats().build());
     return caffeineCacheManager;
   }
 }
