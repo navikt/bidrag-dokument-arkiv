4674cfffa6db9b331987b2c4d477c65b720dd018
Ugur Cenar <cenar93@gmail.com>
2023-08-31 13:19:40 +0200

    Legg til corsfilter

--- src/main/kotlin/no/nav/bidrag/dokument/arkiv/model/Exceptions.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/arkiv/model/Exceptions.kt
@@ -3,9 +3,11 @@ package no.nav.bidrag.dokument.arkiv.model
 import org.springframework.http.HttpStatus
 import org.springframework.web.client.HttpClientErrorException
 
-class ViolationException(violations: MutableList<String>) : RuntimeException("Ugyldige data: ${violations.joinToString(", ")}")
+class ViolationException(violations: MutableList<String>) :
+    RuntimeException("Ugyldige data: ${violations.joinToString(", ")}")
 
-abstract class HttpStatusException(message: String, throwable: Throwable? = null) : RuntimeException(message, throwable) {
+abstract class HttpStatusException(message: String, throwable: Throwable? = null) :
+    RuntimeException(message, throwable) {
     abstract val status: HttpStatus
 }
 
@@ -13,13 +15,16 @@ open class FunksjonellFeilException(message: String) : HttpStatusException(messa
     override val status: HttpStatus get() = HttpStatus.BAD_REQUEST
 }
 
-open class TekniskFeilException(message: String, throwable: Throwable) : HttpStatusException(message, throwable) {
+open class TekniskFeilException(message: String, throwable: Throwable) :
+    HttpStatusException(message, throwable) {
     override val status: HttpStatus get() = HttpStatus.INTERNAL_SERVER_ERROR
 }
 
 class JournalIkkeFunnetException(message: String) : RuntimeException(message)
 class JournalpostDataException(message: String) : RuntimeException(message)
-class JournalpostHendelseException(message: String, throwable: Throwable) : RuntimeException(message, throwable)
+class JournalpostHendelseException(message: String, throwable: Throwable) :
+    RuntimeException(message, throwable)
+
 class JournalpostIkkeFunnetException(message: String) : RuntimeException(message)
 
 class SafException(message: String, override val status: HttpStatus) : HttpStatusException(message)
@@ -36,19 +41,39 @@ class KnyttTilSakManglerTemaException(message: String) : RuntimeException(messag
 
 class UgyldigDistribusjonException(message: String) : FunksjonellFeilException(message)
 class DistribusjonFeiletFunksjoneltException(message: String) : FunksjonellFeilException(message)
-class DistribusjonFeiletTekniskException(message: String, throwable: Throwable) : TekniskFeilException(message, throwable)
+class DistribusjonFeiletTekniskException(message: String, throwable: Throwable) :
+    TekniskFeilException(message, throwable)
 
-class OppdaterJournalpostFeiletFunksjoneltException(message: String) : FunksjonellFeilException(message)
-class OppdaterJournalpostFeiletTekniskException(message: String, throwable: Throwable) : TekniskFeilException(message, throwable)
+class OppdaterJournalpostFeiletFunksjoneltException(message: String) :
+    FunksjonellFeilException(message)
+
+class OppdaterJournalpostFeiletTekniskException(message: String, throwable: Throwable) :
+    TekniskFeilException(message, throwable)
 
 class JournalpostHarIkkeKommetIRetur(message: String) : RuntimeException(message)
 
 class KunneIkkeJournalforeOpprettetJournalpost(message: String) : RuntimeException(message)
-class LagreSaksbehandlerIdentForJournalfortJournalpostFeilet(message: String, throwable: Throwable) : RuntimeException(message, throwable)
-class KunneIkkeKnytteSakTilJournalpost(message: String, throwable: Throwable) : RuntimeException(message, throwable)
+class LagreSaksbehandlerIdentForJournalfortJournalpostFeilet(
+    message: String,
+    throwable: Throwable
+) : RuntimeException(message, throwable)
+
+class KunneIkkeKnytteSakTilJournalpost(message: String, throwable: Throwable) :
+    RuntimeException(message, throwable)
+
 class JournalfortJournalpostManglerJournalfortAvIdent(message: String) : RuntimeException(message)
 
-class HentJournalpostFeilet(message: String, throwable: Throwable) : RuntimeException(message, throwable)
+class HentJournalpostFeilet(message: String, throwable: Throwable) :
+    RuntimeException(message, throwable)
+
+fun fantIkkeDokument(forsendelseId: String, dokumentreferanse: String?): Nothing =
+    throw HttpClientErrorException(
+        HttpStatus.BAD_REQUEST,
+        "Finnes ingen dokument $dokumentreferanse i forsendelse $forsendelseId"
+    )
 
 fun kanIkkeHenteJournalMedUgyldigFagomrade(ugyldigFagomrade: String): Nothing =
-    throw HttpClientErrorException(HttpStatus.BAD_REQUEST, "Kan ikke hente journal med tema $ugyldigFagomrade. Gyldige verdier er BID og FAR")
+    throw HttpClientErrorException(
+        HttpStatus.BAD_REQUEST,
+        "Kan ikke hente journal med tema $ugyldigFagomrade. Gyldige verdier er BID og FAR"
+    )
