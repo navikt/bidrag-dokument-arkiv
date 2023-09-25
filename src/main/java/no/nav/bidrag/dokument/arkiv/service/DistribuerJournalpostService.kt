cb527aacbe4248a128a921ad8a2552c0d8c9027e
Ugur Alpay Cenar <cenar93@gmail.com>
2023-09-25 07:46:42 +0200

    Update log (#415)
    * Log distribusjon av journalpost
    
    * Log distribusjon av journalpost

--- src/main/java/no/nav/bidrag/dokument/arkiv/service/DistribuerJournalpostService.kt
+++ src/main/java/no/nav/bidrag/dokument/arkiv/service/DistribuerJournalpostService.kt
@@ -218,9 +218,7 @@ class DistribuerJournalpostService(
         val distribuerResponse =
             dokdistFordelingConsumer.distribuerJournalpost(journalpost, batchId, adresse)
         LOGGER.info(
-            "Bestillt distribusjon av journalpost {} med bestillingsId {}",
-            journalpostId,
-            distribuerResponse.bestillingsId
+            "Bestillte distribusjon av journalpost $journalpostId med bestillingsId ${distribuerResponse.bestillingsId}, antall dokumenter ${journalpost.dokumenter.size} og kanal ${distribusjonKanal.distribusjonskanal}(${distribusjonKanal.regel}-${distribusjonKanal.regelBegrunnelse})."
         )
 
         // Distribusjonsløpet oppdaterer journalpost og overskriver alt av tilleggsopplysninger. Hent journalpost på nytt for å unngå overskrive noe som distribusjon har lagret
