d5046d1333c22d69a72dd68b6b96c70fa8f5b12e
Ugur Alpay Cenar <cenar93@gmail.com>
2024-08-14 08:39:45 +0200

    Legg til ekstra logging

--- src/main/kotlin/no/nav/bidrag/dokument/arkiv/dto/Saksbehandler.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/arkiv/dto/Saksbehandler.kt
@@ -1,16 +1,11 @@
 package no.nav.bidrag.dokument.arkiv.dto
 
-data class Saksbehandler(
-    var ident: String? = null,
-    var navn: String? = null,
-) {
+data class Saksbehandler(var ident: String? = null, var navn: String? = null) {
     fun hentIdentMedNavn() = "$ident - $navn"
     fun hentSaksbehandlerInfo(saksbehandlerEnhet: String) =
         if (ident == null && navn == null) "ukjent saksbehandler" else hentBrukeridentMedSaksbehandler(saksbehandlerEnhet)
     private fun hentBrukeridentMedSaksbehandler(enhetsnummer: String) = "${navn ?: "Ukjent"} (${ident ?: "Ukjent"} - $enhetsnummer)"
-    fun tilEnhet(enhetsnummer: String?): SaksbehandlerMedEnhet {
-        return SaksbehandlerMedEnhet(this, enhetsnummer ?: "9999")
-    }
+    fun tilEnhet(enhetsnummer: String?): SaksbehandlerMedEnhet = SaksbehandlerMedEnhet(this, enhetsnummer ?: "9999")
 }
 
 data class SaksbehandlerMedEnhet(val saksbehandler: Saksbehandler, val enhetsnummer: String) {
